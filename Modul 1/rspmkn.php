beb8976cf209295e07673e24cc6689daa9acfb31
denny17z <dennykiller48z@yahoo.com>
2020-02-14 09:20:14 +0800

    Modul 1

---
+++ Modul 1/rspmkn.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resep Makanan oleh Denny Indra</title>
+</head>
+<body>
+	<h1 style="color: #8bad00;">Resep Makanan - Rendang Daging Sapi</h1>
+	<img style="width: 550px;" src="1.jpg">
+	<h2 style="color: #ccc9bc;">Bahan - bahan:</h2>
+	<ul>
+		<li>500 gr daging sapi, potong sesuai selera</li>
+		<li>Bumbu Halus:</li>
+		<ol>
+			<li>15 butir bawang merah</li>
+			<li>9 siung bawang putih</li>
+			<li>5 bh cabe merah</li>
+			<li>3 sdm ketumbar bubuk</li>
+			<li>3 ruas lengkuas</li>
+			<li>3 ruas jahe</li>
+		</ol>
+		<li>Bumbu Cemplung:</li>
+		<ol>
+			<li>2 bh sereh, geprek</li>
+			<li>3 lbr daun salam</li>
+			<li>5 lbr daun jeruk</li>
+			<li>3 lbr daun kunyit (bisa d skip)</li>
+			<li>3 sdm air asam jawa</li>
+			<li>2 bh bunga lawang</li>
+			<li>secukupnya Garam gula</li>
+			<li>750 ml santan</li>
+		</ol>
+	</ul>
+	<h2 style="color: #ccc9bc;">Langkah - langkah:</h2>
+	<ul>
+		<li>Bisa dilihat pada link berikut: <a href="https://cookpad.com/id/resep/11563898-rendang-daging-sapi?via=search&search_term=rendang">Resep Makanan - Rendang Daging Sapi oleh Denny Indra</a></li>
+	</ul>
+</body>
+</html>
